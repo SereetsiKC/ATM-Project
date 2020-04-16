060ac48ff8f606db02227339929121ed6b39c3d6
Kopano Conthwait Sereetsi <sereetsikc@hotmail.com>
2020-04-16 22:17:20 +0200

    Question one start up

---
+++ Assignment.one/QuestionOne/Deduction.java
@@ -0,0 +1,10 @@
+
+public enum Deduction {
+    Tax(18), Medical(12.5), CarAllowance(8), UIF(2);
+
+    public final double perceptage;
+
+    private Deduction(final double perceptage) {
+        this.perceptage = perceptage;
+    }
+}
---
+++ Assignment.one/QuestionOne/Details.java
@@ -0,0 +1,60 @@
+
+public class Details {
+    private String employeeIdNumber;
+    private String firstame;
+    private String surname;
+    private double salary;
+    private int increasePercentage = 10;
+
+    public void setEmployeeIdNumber(String newId) {
+        this.employeeIdNumber = newId;
+    }
+
+    public String getEmployeeIdNumber() {
+        return this.employeeIdNumber;
+    }
+
+    public void setFirstName(String newName) {
+        this.firstame = newName;
+    }
+
+    public String getFirstName() {
+        return this.firstame;
+    }
+
+    public void setSurname(String newSurname) {
+        this.surname = newSurname;
+    }
+
+    public String getSurname() {
+        return this.surname;
+    }
+
+    public void setSalary(double newSalary) {
+        this.salary = newSalary;
+    }
+
+    public double getSalary() {
+        return this.salary;
+    }
+
+    public void getUpdatedSalary() {
+        String format = "%-30s%s%n";
+        System.out.println("EMPLOYEE DETAILS REPORT");
+        System.out.println("******************************");
+        System.out.printf(format, "EMPLOYEE NUMBER:", this.employeeIdNumber);
+        System.out.printf(format, "EMPLOYEE FIRST NAME:", this.firstame);
+        System.out.printf(format, "EMPLOYEE SURNAME", this.surname);
+        System.out.printf(format, "ORIGINAL SALARY:", "R" + String.format("%,.2f",this.salary));
+
+        double NewSalary = ((this.increasePercentage + 100) / 100) * this.salary;
+        System.out.printf(format, "INCREASED SALARY:", "R" + String.format("%,.2f",NewSalary));
+
+        double increase = (this.increasePercentage / 100) * this.salary;
+        System.out.printf(format, "INCREASE AMOUNT", "R" + String.format("%,.2f",increase));
+    }
+
+    public void getIncreaseSalaryWithDeductions() {
+
+    }
+}
